407bf235de5fa8813806c8d520ce520aaaf07ad1
Chun chung michael Yuen <chyuen@e1z2r5p39.42.us.org>
2019-06-03 19:27:31 -0700

    flood_fill

--- level4/flood_fill/flood_fill.c
+++ level4/flood_fill/flood_fill.c
@@ -14,19 +14,24 @@
 
 void  flood_fill(char **tab, t_point size, t_point begin)
 {
+	char ch;
+
+	ch = tab[begin.x][begin.y];
 	if (begin.y > size.y || begin.x > size.x || begin.x < 1 || begin.y < 1)
-		return ;
-	if (tab[begin.x][begin.y] == '1')
 		return ;
 	if (tab[begin.x][begin.y] == 'F')
 		return ;
 	tab[begin.x][begin.y] = 'F';
 	if (begin.x != 1)
-		flood_fill(tab, size, (t_point){begin.x - 1, begin.y});
-	if (begin.x != size.x)
-		flood_fill(tab, size, (t_point){begin.x + 1, begin.y});
+		if (tab[begin.x - 1][begin.y] == ch)
+			flood_fill(tab, size, (t_point){begin.x - 1, begin.y});
 	if (begin.y != 1)
-		flood_fill(tab, size, (t_point){begin.x, begin.y - 1});
+		if (tab[begin.x][begin.y - 1] == ch)
+			flood_fill(tab, size, (t_point){begin.x, begin.y - 1});
+	if (begin.x != size.x)
+		if (tab[begin.x + 1][begin.y] == ch)
+			flood_fill(tab, size, (t_point){begin.x + 1, begin.y});	
 	if (begin.y != size.y)
-		flood_fill(tab, size, (t_point){begin.x, begin.y + 1});
+		if (tab[begin.x][begin.y + 1] == ch)
+			flood_fill(tab, size, (t_point){begin.x, begin.y + 1});
 }
